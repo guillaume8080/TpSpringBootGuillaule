3dcef452832403397b0af7ff2a7175e6139e205b
Guillaume Gouy <guillaumegouy@sfr.fr>
2021-01-11 10:21:06 +0100

    projetDeclared

---
+++ src/main/java/com/tactfactory/microservice/cloudconfig/MicroserviceCloudconfigApplication.java
@@ -0,0 +1,13 @@
+package com.tactfactory.microservice.cloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCloudconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCloudconfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tactfactory/microservice/cloudconfig/MicroserviceCloudconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tactfactory.microservice.cloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCloudconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
